634b42bd868d3f30fabf914e721ece666c7495b2
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-09-21 20:11:48 -0300

    feat :sparkles: Transformação:  lê os dados e fiiltra os artistas mais escutados

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import pandas as pd
 
-#Extração:
+# Extration:
 data = pd.read_csv('data/spotify_most_streamed.csv')
 
-print(data)
+# Transformation:
+data['Artist'] = data['Artist and Title'].str.split('-').str[0]
+
+somas_por_artista = {}
+
+for artist in data['Artist'].unique():
+
+    filtro = data['Artist'] == artist
+    
+    data.loc[filtro, 'Streams'] = data.loc[filtro, 'Streams'].str.replace(',', '', regex=True).astype(float)
+    
+    soma_streams = data[filtro]['Streams'].sum()
+    
+    somas_por_artista[artist] = soma_streams
+
+for artist, soma in somas_por_artista.items():
+    print(f'Artista: {artist}, Soma das Streams: {soma}')
